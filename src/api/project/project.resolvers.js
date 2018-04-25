4bbe487bdec3a914e51df5edc126e5911560492a
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-04-25 13:21:55 -0500

    lesson 2 wip

--- src/api/project/project.resolvers.js
+++ src/api/project/project.resolvers.js
@@ -1,35 +1,42 @@
 const project = async (_, args, ctx, info) => {
-  const project = await ctx.models.project
-    .findById(args.input.id)
-    .exec()
+    const project = await ctx.models.project
+        .findById(args.input.id)
+        .exec()
 
-  if (!project) {
-    throw new Error('Project does not exist')
-  }
+    if (!project) {
+        throw new Error('Project does not exist')
+    }
 
-  return project
+    return project
 }
 
 const projects = (_, __, ctx) => {
-  return ctx.models.project.find({}).exec()
+    return ctx.models.project.find({}).exec()
 }
 
 const newProject = (_, args, ctx) => {
-  return ctx.models.project.create(args.input)
+    console.log('project resolver')
+    return ctx.models.project.create(args.input)
 }
 
 module.exports = {
-  Query: {
-    project,
-    projects
-  },
-  Mutation: {
-    newProject
-  },
-  Project: {
-    id(project) {
-      return project._id + ''
+    Query: {
+        project,
+        projects
     },
-    // resolve some fields here
-  }
+    Mutation: {
+        newProject
+    },
+    Project: {
+        id(project) {
+            console.log("id resolver")
+            return project._id + ''
+        },
+        tasks(project, args, ctx) {
+            console.log("task resolver")
+            return ctx.models.task
+                .find({project: project._id})
+                .exec()
+        }
+    }
 }
--- src/api/task/task.resolvers.js
+++ src/api/task/task.resolvers.js
@@ -1,50 +1,59 @@
 const {reposForOrg} = require('../github')
 
 const task = (_, args, ctx) => {
-  const {id: _id, name, project} = args.input
+    const {id: _id, name, project} = args.input
 
-  if (!_id && !name) {
-    throw new Error('Invalid input')
-  }
-  return ctx.loaders.task.load(_id)
+    if (!_id && !name) {
+        throw new Error('Invalid input')
+    }
+    return ctx.loaders.task.load(_id)
 }
 const tasks = (_, args, ctx) => {
-  return ctx.models.task.find(args.input)
+    return ctx.models.task.find(args.input)
 }
 const newTask = (_, args, ctx) => {
-  return ctx.models.task.create(args.input)
+    return ctx.models.task.create(args.input)
 }
 const removeTask = async (_, args, ctx) => {
-  const task = await ctx.models.task
-    .findByIdAndRemove(args.id)
-    .exec()
-  
-  if (!task) {
-    throw new Error('No resource')
-  }
-  return task
+    const task = await ctx.models.task
+        .findByIdAndRemove(args.id)
+        .exec()
+
+    if (!task) {
+        throw new Error('No resource')
+    }
+    return task
 }
 const changeStatus = (_, args, ctx) => {
-  return ctx.models.task.findByIdAndUpdate(args.input.id, {
-    status: args.input.status
-  }, {new: true})
-  .exec()
+    return ctx.models.task.findByIdAndUpdate(args.input.id, {
+        status: args.input.status
+    }, {new: true})
+        .exec()
 }
 
 module.exports = {
-  Query: {
-    task,
-    tasks
-  },
-  Mutation: {
-    newTask,
-    changeStatus,
-    removeTask
-  },
-  Task: {
-    id(task) {
-      return task._id + ''
+    Query: {
+        task,
+        tasks
     },
-    // resolve some fields here
-  }
+    Mutation: {
+        newTask,
+        changeStatus,
+        removeTask
+    },
+    Task: {
+        id(task) {
+            return task._id + ''
+        },
+        project(task, args, ctx) {
+            return ctx.models.project
+                .findById(task.project)
+                .exec()
+        },
+        parentTask(task, args, ctx) {
+            return ctx.models.task
+                .findById(task.parentTask)
+                .exec()
+        }
+    }
 }
